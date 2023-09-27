6f57d6f306a56c4eecb5c27d30e4aee839b6551a
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-09-27 09:51:08 +0100

    added starting quotes and JS logic

---
+++ main.js
@@ -0,0 +1,21 @@
+let wisdomArray = [
+  "“That\'s dynamic, attacking rugby. And the result is three points.”",
+  "“It's sunday. If you want to go to church, go there and worship Brian O'driscoll,”",
+  " “Richie was one of the greatest referees of the 21st century,”",
+  "“He came from mozambique then, running from deep,”",
+  "“Australia without Pocock is like Hamlet without the prince,”",
+  "“You have to play well if you\'re dressed like that,”",
+  "“It\'s not his physical presence that makes him such a threat, it\'s the magic in his boots,”",
+  "“You don\'t need tries to win World Cups, just points,”",
+  "“Kurtley Beale is prowling, Brad Barritt is bleeding”",
+  "“Who is this team in white?”",
+];
+
+const button = document.querySelector(".button-click");
+const pearlOfWisdom = document.getElementById("pearl-of-wisdom");
+
+button.addEventListener("click", function () {
+  const randomIndex = Math.floor(Math.random() * wisdomArray.length);
+  const randomWisdom = wisdomArray[randomIndex];
+  pearlOfWisdom.textContent = randomWisdom;
+});
